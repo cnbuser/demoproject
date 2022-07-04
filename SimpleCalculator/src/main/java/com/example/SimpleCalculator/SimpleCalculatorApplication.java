08ab9d66b18fe4baf67e4b446baf3038d390c33d
Chandranath <Chandranath@DESKTOP-CDT700D>
2022-07-04 18:43:24 +0530

    new changes

--- SimpleCalculator/src/main/java/com/example/SimpleCalculator/SimpleCalculatorApplication.java
+++ SimpleCalculator/src/main/java/com/example/SimpleCalculator/SimpleCalculatorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleCalculatorApplication {
-	/////
+	/////main application
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleCalculatorApplication.class, args);
--- SimpleCalculator/src/main/java/com/example/SimpleCalculator/controller/MathController.java
+++ SimpleCalculator/src/main/java/com/example/SimpleCalculator/controller/MathController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 	
-	////
+	////main controller
 
 	@GetMapping("/checkapplication")
 	public String saveBook() {
