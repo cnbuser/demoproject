eecc51c166cda23e3aac20ab6ec6fe2fa98179b9
Chandranath <Chandranath@DESKTOP-CDT700D>
2022-07-04 18:05:27 +0530

    new changes

--- SimpleCalculator/src/main/java/com/example/SimpleCalculator/controller/MathController.java
+++ SimpleCalculator/src/main/java/com/example/SimpleCalculator/controller/MathController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MathController {
+	
+	////
 
 	@GetMapping("/checkapplication")
 	public String saveBook() {
